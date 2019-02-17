0d0d0e080a11f92282445c90970a5a6a01d2a72c
Maggie Davidson <jmagdavidson@gmail.com>
2019-02-17 14:47:52 -0500

    Adding routes to proj2 py file

--- SI507_project2.py
+++ SI507_project2.py
@@ -1,18 +1,29 @@
 from flask import Flask
 from movie_tools import Movie
-import csv
+import csv, random
 
 app = Flask(__name__)
 
 @app.route('/')
 def count_movies():
+    return '<h1 style="color:FireBrick;font-family:arial;">{} movies recorded.</h1>'.format(len(movie_instances))
+
+@app.route('/movies/ratings/')
+def show_ratings():
+    random.shuffle(movie_instances)
+    movie_rating_string = ''
+    for num in range(10):
+        movie_rating_string = movie_rating_string + movie_instances[num].getIMDBRatingString() + '<br>'
+    return '<h3 style="color:MediumAquaMarine;font-family:arial;">{}</h3>'.format(movie_rating_string)
+
+if __name__ == '__main__':
     with open("movies_clean.csv", "r") as movies_file:
         movie_lists = []
         moviereader = csv.reader(movies_file)
         next(moviereader)
         for row in moviereader:
             movie_lists.append(row)
-    return '<h1>{} movies recorded.</h1>'.format(len(movie_lists))
-
-if __name__ == '__main__':
+        movie_instances = []
+        for movie in movie_lists:
+            movie_instances.append(Movie(movie))
     app.run()
